eb703538e5832c4ed8c608adb4f1f658e4e786a5
kwishna <kwishna@gmail.com>
2024-05-13 23:26:15 +0530

    data analysis

--- src/langchain_apis/excel-analysis.py
+++ src/langchain_apis/excel-analysis.py
@@ -1,16 +1,25 @@
-import os, pandasai, pandas
-from langchain_community.llms import OpenAI
-from langchain_openai.llms import OpenAI
+import pandas
+import pandasai
 from dotenv import load_dotenv
+from langchain_experimental.agents import create_csv_agent
+from langchain_openai.llms import OpenAI
 
 load_dotenv()
 
 llm = OpenAI()
-llm.model_name="gpt-3.5-turbo-instruct"
+llm.model_name = "gpt-3.5-turbo-instruct"
 
-df = pandas.read_csv("./sat_results.csv")
-sdf = pandasai.SmartDataframe(df, config={"llm": llm})
+print("--------------------------------------------------------")
 
-sdf.verbose = True
-ans = sdf.chat("How many unique 'DBN' are there?")
-print(ans)
+# df = pandas.read_csv("./sat_results.csv")
+# sdf = pandasai.SmartDataframe(df, config={"llm": llm})
+#
+# sdf.verbose = True
+# ans = sdf.chat("How many unique 'DBN' are there?")
+# print(ans)
+
+print("--------------------------------------------------------")
+
+agent = create_csv_agent(llm=llm, path=['./sat_results.csv'], verbose=True)
+res = agent.invoke({"input": "How many rows of data do you have?"})
+print(res)
