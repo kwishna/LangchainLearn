78523264e562127ac80b4a7dfc5e5eea7edb32fe
kwishna <kwishna@gmail.com>
2024-05-13 22:27:11 +0530

    openai new upgrade

--- src/langchain_apis/openai-001.py
+++ src/langchain_apis/openai-001.py
@@ -1,24 +1,29 @@
 import os
 import openai
+from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()  # read local .env file
 openai.api_key = os.environ['OPENAI_API_KEY']
 
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+
 
 # -------- 1 --------
 def get_completion(prompt, model="gpt-3.5-turbo"):
     _messages = [{"role": "user", "content": prompt}]
-    _response = openai.ChatCompletion.create(
+    _response = client.chat.completions.create(
         model=model,
         messages=_messages,
         temperature=0,
     )
-    return _response.choices[0].message["content"]
+
+    print(_response)
+    return _response.choices[0].message.content
 
 
-print(get_completion("Q. What is 1+1?\n A."))
-print("-"*20)
+print(get_completion("Q. What is 1+1?"))
+print("-------------------------------------------------------")
 print(get_completion("What is 1+1?"))
 
 # ------------- 2 ----------
